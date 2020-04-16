ea07c0d38fa1d7d20c1790d0fad9b8ddddad47f6
Michael Piercy <michael.piercy@zoho.com>
2020-04-16 01:16:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,7 @@ def add_element_to_start_of_array(array, element)
   kids.unshift("wow")
 end
 
-def remove_element_from_end_of_array(array)
+def remove_element_from_end_of_array(kids)
   kids = ["Matt", "Mikey", "Jason", "Jake"]
   kids.pop
 end
