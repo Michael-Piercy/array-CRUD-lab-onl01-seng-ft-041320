9932412651c9294a9813c578d0a4e6dc8596f5d0
Michael Piercy <michael.piercy@zoho.com>
2020-04-16 00:56:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   kids = ["Matt", "Mikey", "Jason", "Jake"]
-  kids << "Heather"
+  kids << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
